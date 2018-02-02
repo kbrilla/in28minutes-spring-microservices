e64b3bd33441aa37d22cdd720932f870d47f6307
Krzysztof Brilla <krzysztof.brilla@nordea.com>
2018-02-02 11:16:42 +0100

    swagger2 implementation

---
+++ restful-web-services/src/main/java/in28minutes/rest/webservices/restfulwebservices/config/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package in28minutes.rest.webservices.restfulwebservices.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2);
+    }
+}
