bd219a841e0f74c085b11f36eed4a5a56df0eeff
Krzysztof Brilla <krzysztof.brilla@nordea.com>
2018-02-02 14:00:06 +0100

    endpoints configuration

---
+++ restful-web-services/src/main/java/in28minutes/rest/webservices/restfulwebservices/config/ActuatorConfig.java
@@ -0,0 +1,8 @@
+package in28minutes.rest.webservices.restfulwebservices.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ActuatorConfig {
+}
