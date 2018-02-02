e8ab8f9a9776a3d579011ebda8d74d07ebbb4ddb
Krzysztof Brilla <krzysztof.brilla@nordea.com>
2018-02-02 10:52:47 +0100

    application/xml ACCEPT-LOCALE

--- restful-web-services/src/main/java/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ restful-web-services/src/main/java/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -2,11 +2,33 @@ package in28minutes.rest.webservices.restfulwebservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestfulWebServicesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulWebServicesApplication.class, args);
+    }
+
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
+
+    // we have to give name to Bean() or name function as messageSource
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("messages");
+        return messageSource;
+    }
 }
--- restful-web-services/src/main/java/in28minutes/rest/webservices/restfulwebservices/controller/HelloWorldController.java
+++ restful-web-services/src/main/java/in28minutes/rest/webservices/restfulwebservices/controller/HelloWorldController.java
@@ -1,12 +1,20 @@
 package in28minutes.rest.webservices.restfulwebservices.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController()
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
+
     @GetMapping("hello-world")
-    public String helloWorld(){
-        return "hello world";
+    public String helloWorld(@RequestHeader(value = "Accept-Language", required = false) Locale locale){
+        return messageSource.getMessage("good.morning.message", null,  locale);
     }
 
     @GetMapping("hello-world-bean")
